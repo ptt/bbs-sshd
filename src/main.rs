57b32f9de0ab7b7945bdef68a7db95eb1f7924d9
Robert <robert@arctic.tw>
2021-05-29 20:21:59 -0700

    Log to syslog

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,56 @@ mod telnet;
 use log::info;
 use std::net::SocketAddr;
 use std::str::FromStr;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
+use syslog::{Facility, Formatter3164, LoggerBackend};
 use tokio::signal::unix::{signal, SignalKind};
+
+struct Logger {
+    under: Arc<Mutex<syslog::Logger<LoggerBackend, Formatter3164>>>,
+}
+
+impl Logger {
+    fn new(under: syslog::Logger<LoggerBackend, Formatter3164>) -> Self {
+        Logger {
+            under: Arc::new(Mutex::new(under)),
+        }
+    }
+}
+
+impl log::Log for Logger {
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
+
+    fn log(&self, record: &log::Record) {
+        if self.enabled(record.metadata()) {
+            let mut under = self.under.lock().unwrap();
+            let message = format!("{} ({}) {}", record.level(), record.target(), record.args());
+            let _ = match record.level() {
+                log::Level::Error => under.err(message),
+                log::Level::Warn => under.warning(message),
+                log::Level::Info => under.info(message),
+                log::Level::Debug => under.debug(message),
+                log::Level::Trace => under.debug(message),
+            };
+        }
+    }
+
+    fn flush(&self) {}
+}
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    let logger = syslog::unix(Formatter3164 {
+        facility: Facility::LOG_LOCAL0,
+        hostname: None,
+        process: "bbs-sshd".into(),
+        pid: std::process::id() as i32,
+    })
+    .expect("unable to start logging");
+    log::set_boxed_logger(Box::new(Logger::new(logger))).unwrap();
+    log::set_max_level(log::LevelFilter::Info);
 
     let mut config = thrussh::server::Config::default();
     config.server_id = "SSH-2.0-bbs-sshd".to_string();
